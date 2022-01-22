82ab09cb25b5d52c834c7764fabeb6052b906e30
Geraldo Quende <adolfoquende@hotmail.com>
2022-01-22 15:07:36 +0100

    init

---
+++ src/main/java/com/vipas/gquendelibray/GquendelibrayApplication.java
@@ -0,0 +1,13 @@
+package com.vipas.gquendelibray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GquendelibrayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GquendelibrayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vipas/gquendelibray/GquendelibrayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vipas.gquendelibray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GquendelibrayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
