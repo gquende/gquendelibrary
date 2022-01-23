6f02de3a8ba1473ce72cea1acbe035c03c185383
Geraldo Quende <adolfoquende@hotmail.com>
2022-01-23 21:10:40 +0100

    Import some class

--- src/main/java/com/vipas/gquendelibray/configs/Configurations.java
+++ src/main/java/com/vipas/gquendelibray/configs/Configurations.java
@@ -1,5 +1,8 @@
 package com.vipas.gquendelibray.configs;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
