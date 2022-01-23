2c2cc35ece356b656c912c4cdb7a773a125cb5c4
Geraldo Quende <adolfoquende@hotmail.com>
2022-01-23 21:21:55 +0100

    Created Book Entity, LibraryRestController, BookRepository

--- src/main/java/com/vipas/gquendelibray/controllers/LibraryController.java
+++ src/main/java/com/vipas/gquendelibray/controllers/LibraryController.java
@@ -7,13 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LibraryController {
 
-
-
-
 @RequestMapping("/")
 public String home(){
     return "home";
 }
+
 
 
 
