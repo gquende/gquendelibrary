31d0969cc01d0618b75be908443f8dc396141232
Geraldo Quende <adolfoquende@hotmail.com>
2022-02-08 23:38:12 +0100

    Follow de Course Spring Fundamentals and G Unit

--- src/main/java/com/vipas/gquendelibray/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vipas.gquendelibray.repository;
-
-import com.vipas.gquendelibray.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
