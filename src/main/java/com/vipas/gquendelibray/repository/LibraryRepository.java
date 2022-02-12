aceb06d8cdb453b4fa755b3c27daaf73f85a4f7b
Geraldo Quende <adolfoquende@hotmail.com>
2022-02-12 16:39:45 +0100

    changes

--- src/main/java/com/vipas/gquendelibray/repository/LibraryRepository.java
+++ src/main/java/com/vipas/gquendelibray/repository/LibraryRepository.java
@@ -4,6 +4,6 @@ import com.vipas.gquendelibray.models.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+//@Repository
 public interface LibraryRepository extends JpaRepository<Library, String> {
 }
