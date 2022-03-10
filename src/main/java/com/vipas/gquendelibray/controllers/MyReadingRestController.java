801acd5fafc149a4c01d1f2b758b9e17f25e07d1
Geraldo Quende <adolfoquende@hotmail.com>
2022-03-10 18:49:57 +0100

    getReading and getAllReadings Method build

--- src/main/java/com/vipas/gquendelibray/controllers/MyReadingRestController.java
+++ src/main/java/com/vipas/gquendelibray/controllers/MyReadingRestController.java
@@ -9,42 +9,64 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/myreading/api")
 public class MyReadingRestController {
 
-//@Autowired
-//    MyReadingServices myReadingServices;
-
-@Autowired
+    @Autowired
     MyReadingRepository myReadingRepository;
 
-
     @PostMapping("/reading/new")
-    public ResponseEntity addReading(@RequestBody MyReading myReading){
-       myReadingRepository.save(myReading);
+    public ResponseEntity addReading(@RequestBody MyReading myReading)
+    {
+        myReadingRepository.save(myReading);
         return new ResponseEntity("Saved Successful", HttpStatus.CREATED);
 
     }
 
+
     @GetMapping("/reading/{id}")
-    public ResponseEntity getReading(@PathVariable("id") long id){
+    public ResponseEntity getReading(@PathVariable("id") long id)
+    {
         Optional<MyReading> optional= myReadingRepository.findById(id);
-
-        if (optional.isPresent()){
-
+        if (optional.isPresent())
+        {
             return new ResponseEntity(optional.get(),HttpStatus.OK);
         }
-        else {
-            return new ResponseEntity("Object not Found", HttpStatus.NOT_FOUND);
+        else
+        {
+            return new ResponseEntity("Object not Found",HttpStatus.NOT_FOUND);
         }
 
+    }
 
-
+    @GetMapping("/reading/all")
+    public ResponseEntity getAllReadings()
+    {
+        List<MyReading> myReadingList=myReadingRepository.findAll();
+        return new ResponseEntity(myReadingList,HttpStatus.OK);
 
     }
+
+    @DeleteMapping("/reading/{id}")
+    public ResponseEntity deleteReading(@PathVariable("id") long id)
+    {
+        Optional<MyReading> optional=myReadingRepository.findById(id);
+        if (optional.isPresent()){
+            return new ResponseEntity("",HttpStatus.OK);
+        }
+        else{
+            return  new ResponseEntity("Object not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+//    @PutMapping("/reading/update/{id}")
+//    public ResponseEntity updateReading(@RequestBody MyReading myReading){
+//
+//    }
 
 
 }
